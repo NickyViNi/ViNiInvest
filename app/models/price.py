34b3a125b5fe2db2a54071ed33a6c66b156cc1bf
NickyViNi <happylemonlcm@gmail.com>
2024-02-28 01:42:53 -0800

    price modal

---
+++ app/models/price.py
@@ -0,0 +1,17 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+
+class Price(db.Model):
+
+    __tablename__ = "prices"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    stock_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("stocks.id")), nullable=False)
+    max_price = db.Column(db.Float, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    min_price = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime)
