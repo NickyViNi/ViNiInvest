67bbb4aae1d469657c780e2b1ace23d13006e304
NickyViNi <happylemonlcm@gmail.com>
2024-02-27 22:33:50 -0800

    user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,8 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
+from sqlalchemy import validates
 
 
 class User(db.Model, UserMixin):
@@ -10,9 +12,51 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    profile_image_url = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+
+    @validates('first_name')
+    def validate_first_name(self, _, val):
+        if not len(val):
+            raise ValueError({ "first_name": "First name is required" })
+        return val
+
+
+    @validates('last_name')
+    def validate_last_name(self, _, val):
+        if not len(val):
+            raise ValueError({"last_name": "Last name is required"})
+        return val
+
+
+    @validates('username')
+    def validate_username(self, _, val):
+        if len(val) < 4:
+            raise ValueError({"username": "Username must be at least 4 characters"})
+        if len([user for user in User.query.all() if user.username == val]):
+            raise ValueError({ "username": "User with that username already exists" })
+        return val
+
+
+    @validates("email")
+    def validate_email(self, _, val):
+        if "@" not in val:
+            raise ValueError({"message": "Invalid email"})
+        if len([user for user in User.query.all() if user.email == val]):
+            raise ValueError({ "email": "User with that email already exists" })
+        return val
+
+
+    @classmethod
+    def username_to_ids(cls):
+        return { user.username: user.id for user in cls.query.all() }
 
     @property
     def password(self):
@@ -27,7 +71,10 @@ class User(db.Model, UserMixin):
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "username": self.username,
+            "email": self.email,
+            "profile_image_url": self.profile_image_url,
         }
