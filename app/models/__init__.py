41d8bb43ccb48d2b7d93d7922bc992d55535f13d
NickyViNi <happylemonlcm@gmail.com>
2024-02-28 02:04:15 -0800

    migrations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,9 @@
 from .db import db, environment, SCHEMA
 from .user import User
 from .portfolio import Portfolio
+from .stock import Stock
+from .transaction import Transaction
+from .portfolio_stock import Portfolio_stock
+from .price import Price
+from .watchlist import Watchlist
+from .watchlist_stock import Watchlist_stock
