3e52f3eaf9e2f6496d6f9d46f87d76ed4fcfc9c8
NickyViNi <happylemonlcm@gmail.com>
2024-02-28 14:28:57 -0800

    transaction form

---
+++ app/forms/transaction_form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, FloatField, BooleanField, SelectField
+from wtforms.validators import DataRequired, ValidationError
+
+
+def share_check(form, field):
+    if field.data <= 0:
+        raise ValidationError("Share must be grater than 0")
+
+def type_check(form, field):
+    if field.data.lower() not in ["sell", "buy"]:
+        raise ValidationError("Transaction type must be sell or buy")
+
+class TransactionForm(FlaskForm):
+    shares = FloatField("Shares", validators=[DataRequired(), share_check])
+    type = SelectField("Type", choices=["sell", "buy"], coerce=str, validate_choice=True, validators=[DataRequired(), type_check])
