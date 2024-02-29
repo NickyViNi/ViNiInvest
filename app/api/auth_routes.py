273539e160777419722d6bf66bcbcc6eeb5c2acf
NickyViNi <happylemonlcm@gmail.com>
2024-02-29 14:35:29 -0800

    login with email or username

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -41,7 +41,7 @@ def login():
 
     if form.validate_on_submit():
         # Add the user to the session, we are logged in!
-        user = User.query.filter(User.email == form.data['email_username'] or User.username == form.data['email_username']).first()
+        user = User.query.filter((User.email == form.data['email_username']) | (User.username == form.data['email_username'])).first()
 
         login_user(user)
 
