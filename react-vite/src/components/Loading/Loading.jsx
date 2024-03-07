e6271d80c586653dafbb79ff7035998a085da60c
NickyViNi <happylemonlcm@gmail.com>
2024-03-07 00:50:17 -0800

    loading animation fixed

--- react-vite/src/components/Loading/Loading.jsx
+++ react-vite/src/components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import "./Loading.css";
 export default function Loading() {
   return (
     <div className="loading">
-      <div className="loader"></div>
+      <div className="wait-loader"></div>
     </div>
   );
 }
