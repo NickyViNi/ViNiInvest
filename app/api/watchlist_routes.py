0cd87aba9f2ec91c2d59f696686788eacad26df2
NickyViNi <happylemonlcm@gmail.com>
2024-03-08 11:23:02 -0800

    watchlist routes

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, redirect
 from flask_login import login_required, current_user
+from ..models import db, Watchlist
 from ..forms import WatchlistForm
 
 watchlist_routes = Blueprint("watchlists", __name__)
@@ -7,4 +8,28 @@ watchlist_routes = Blueprint("watchlists", __name__)
 @watchlist_routes.route("/")
 @login_required
 def watchlists():
+    """Get all watchlists owned by current user"""
+    user_watchlists = [watchlist.to_dict() for watchlist in current_user.watchlists]
+    return user_watchlists
+
+
+@watchlist_routes.route("/<int:id>")
+@login_required
+def get_watchlist():
+    pass
+
+
+@watchlist_routes.route("/", methods=["POST"])
+@login_required
+def create_watchlist():
+    pass
+
+@watchlist_routes.route("/<int:id>", methods=["PUT"])
+@login_required
+def update_watchlist():
+    pass
+
+@watchlist_routes.route("/", methods=["DELETE"])
+@login_required
+def delete_watchlist():
     pass
