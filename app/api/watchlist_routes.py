4470e9825cf811ecb7306df08c873e8a7f536067
NickyViNi <happylemonlcm@gmail.com>
2024-03-08 17:04:13 -0800

    update and delete watchlist routes

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, redirect
 from flask_login import login_required, current_user
-from ..models import db, Watchlist
+from ..models import db, Watchlist, Watchlist_stock, Stock
 from ..forms import WatchlistForm
 
 watchlist_routes = Blueprint("watchlists", __name__)
@@ -24,7 +24,7 @@ def get_watchlist(id):
     if watchlist.user_id != current_user.id:
         return redirect("/api/auth/forbidden")
 
-    return watchlist.to_dict(watchlist_stocks=False), 200
+    return watchlist.to_dict(watchlist_stocks=True), 200
 
 
 @watchlist_routes.route("/", methods=["POST"])
@@ -48,25 +48,74 @@ def create_watchlist():
 
 @watchlist_routes.route("/<int:watchlist_id>/stocks/<int:stock_id>", methods=["POST"])
 @login_required
-def add_stock_to_watchlist():
+def add_stock_to_watchlist(watchlist_id, stock_i):
     """addind a stock into a watchlist by id"""
-    pass
+    watchlist = Watchlist.query.get(watchlist_id)
+    new_watchlist_stock = Watchlist_stock (
+        watchlist_id = watchlist_id,
+        stock_id = stock_i
+    )
+    db.session.add(new_watchlist_stock)
+    db.session.commit()
+    return watchlist.to_dict(watchlist_stocks=True), 201
 
 
 @watchlist_routes.route("/<int:id>", methods=["PUT"])
 @login_required
 def update_watchlist(id):
     """update a watchlist name by id"""
-    pass
+    form = WatchlistForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    watchlist = Watchlist.query.get(id)
+
+    if not watchlist:
+        return {"message": "Watchlist couldn't be found"}, 404
+    if watchlist.user_id != current_user.id:
+        return redirect("/api/auth/forbidden")
+
+    if form.validate_on_submit():
+        if form.data["name"] != watchlist.name:
+            watchlist_in_db = Watchlist.query.filter(Watchlist.name == form.data["name"]).first()
+            if watchlist_in_db:
+                return {"name":  "This name is already taken"}, 409
+            watchlist.name = form.data["name"]
+            db.session.commit()
+        return watchlist.to_dict(watchlist_stocks=True), 200
+    return form.errors, 400
+
 
 @watchlist_routes.route("/<int:id>", methods=["DELETE"])
 @login_required
 def delete_watchlist(id):
     """delete a watchlist by id"""
-    pass
+    watchlist = Watchlist.query.get(id)
+
+    if not watchlist:
+        return {"message": "Watchlist couldn't be found"}, 404
+    if watchlist.user_id != current_user.id:
+        return redirect("/api/auth/forbidden")
+
+    db.session.delete(watchlist)
+    db.session.commit()
+
+    return {"message": "Successfully deleted watchlist"}, 200
+
 
 @watchlist_routes.route("/<int:watchlist_id>/stocks/<int:stock_id>", methods=["DELETE"])
 @login_required
 def delete_watchlist(watchlist_id, stock_id):
     """delete a stock from a watchlist by id"""
-    pass
+    watchlist = Watchlist_stock.query.get(watchlist_id)
+    stock = Stock.query.get(stock_id)
+    if not watchlist:
+        return {"message": "Watchlist couldn't be found"}, 404
+    if not stock:
+        return {"message": "Stock couldn't be found"}, 404
+    watchlist_stock = Watchlist_stock.query.filter(Watchlist_stock.watchlist_id == watchlist.id).filter(Watchlist_stock.stock_id == stock.id)
+    if not watchlist_routes:
+        return {"message": "Stock couldn't be found in this watchlist"}, 404
+
+    db.session.delete(watchlist_stock)
+    db.session.commit()
+
+    return {"message": "Successfully remove the stock from this watchlist"}, 200
