eebb2f5854f7189c83f466c79367249d7142e905
NickyViNi <happylemonlcm@gmail.com>
2024-03-13 22:49:02 -0700

    recommend statistics

--- react-vite/src/helpers/recommendationCalculate.js
+++ react-vite/src/helpers/recommendationCalculate.js
@@ -1,3 +1,16 @@
 export const recommendationCalculate = (analyses) => {
+    const recommend = { "Buy": 0, "Sell": 0, "Hold": 0 };
+    analyses?.forEach(a => {
+        if (a.recommendation.toLowerCase() === "buy") {
+            recommend.Buy += 1
+        }
+        if (a.recommendation.toLowerCase() === "sell") {
+            recommend.Sell += 1
+        }
+        if (a.recommendation.toLowerCase() === "hold") {
+            recommend.Hold += 1
+        }
+    })
 
+    return recommend;
 }
