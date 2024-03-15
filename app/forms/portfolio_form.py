720882fedf82bf1d5b785277aa2a57cee9022213
NickyViNi <happylemonlcm@gmail.com>
2024-03-15 09:02:49 -0700

    limit deposit

--- app/forms/portfolio_form.py
+++ app/forms/portfolio_form.py
@@ -11,6 +11,8 @@ def money_check_amount(form, field):
     #     raise ValidationError("Money must be a positive number")
     if field.data < 0:
         raise ValidationError("Money shouldn't be less than 0")
+    if field.data > 1000000:
+        raise ValidationError("You can only deposit $1,000,000 at one time")
 
 class PortfolioForm(FlaskForm):
     name = StringField("Portfolio Name", validators=[DataRequired(), name_check_len])
